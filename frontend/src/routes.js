24a23a8203d25822a8dc9fa2b3135908cff26f5d
hype1356 <swayamkan@gmail.com>
2024-10-26 12:10:57 +0100

    Get rid of unnecessary pages

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -20,14 +20,14 @@ import RTL from 'views/admin/rtl';
 import SignInCentered from 'views/auth/signIn';
 
 const routes = [
-  {
+  /*{
     name: 'Main Dashboard',
     layout: '/admin',
     path: '/default',
     icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
     component: <MainDashboard />,
-  },
-  {
+  },*/
+  /*{
     name: 'NFT Marketplace',
     layout: '/admin',
     path: '/nft-marketplace',
@@ -41,7 +41,7 @@ const routes = [
     ),
     component: <NFTMarketplace />,
     secondary: true,
-  },
+  },*/
   {
     name: 'Data Tables',
     layout: '/admin',
@@ -62,13 +62,6 @@ const routes = [
     path: '/sign-in',
     icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
     component: <SignInCentered />,
-  },
-  {
-    name: 'RTL Admin',
-    layout: '/rtl',
-    path: '/rtl-default',
-    icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
-    component: <RTL />,
   },
 ];
 
